7278331f702145a3fa6ff45666aa928ce2ee4bd8
ProtoSD <sdproto@gmail.com>
2012-09-23 01:25:40 -0700

    Thought /var/subsonic/transcode directory was created by default, linking libraries failed because its not

--- nanobsd/plugins/subsonic_pbi/scripts/post-install.sh
+++ nanobsd/plugins/subsonic_pbi/scripts/post-install.sh
@@ -26,6 +26,7 @@ ln -sf ${SUBSONIC_HOME}/etc/rc.d/tomcat6 /usr/local/etc/rc.d/
 #/usr/pbi/subsonic-amd64/etc/rc.d
 #mv ${SUBSONIC_HOME}/subsonic_47.war ${SUBSONIC_HOME}/apache-tomcat-6.0/webapps/subsonic.war
 
+mkdir -p /var/subsonic/transcode
 ln -sf ${SUBSONIC_HOME}/bin/lame /var/subsonic/transcode/lame
 ln -sf ${SUBSONIC_HOME}/bin/lame /var/subsonic/transcode/
 ln -sf ${SUBSONIC_HOME}/bin/ffmpeg /var/subsonic/transcode/ffmpeg
